deac4d7bd9bea0fe177cf3e1995a95024ea8cba9
eric phung <esphung@users.noreply.github.com>
2023-04-01 18:05:21 -0500

    Update HTTPNetworkRoutes.swift

--- Networking/HTTP/HTTPNetworkRoutes.swift
+++ Networking/HTTP/HTTPNetworkRoutes.swift
@@ -5,5 +5,7 @@ public enum HTTPNetworkRoute: String{
     case createPost = "posts/new"
     case editPost = "posts/:id/edit"
     case deletePost = "posts/:id"
+
+    case createToken = "api/teamchat/mobile/amity/auth/:staff_id"
     // You can add as many routes/endpoints as you have/want
 }
